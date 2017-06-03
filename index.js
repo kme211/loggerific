900276c3aba2274797ede52c39504ff2ca31831f
Keari Eggers <keari@Kearis-MacBook-Air.local>
2017-06-03 17:52:51 -0500

    remove old stuff

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
-const join = require('path').join;
+const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 const express = require('express');
 const session = require('express-session');
@@ -10,12 +10,11 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const bodyParser = require('body-parser');
-const models = join(__dirname, 'models');
-const getFormattedDate = require('./getFormattedDate');
+const models = path.join(__dirname, 'models');
 // Bootstrap models
 fs.readdirSync(models)
   .filter(file => ~file.search(/^[^\.].*\.js$/))
-  .forEach(file => require(join(models, file)));
+  .forEach(file => require(path.join(models, file)));
 const routes = require('./routes/index');
 const helpers = require('./helpers');
 const errorHandlers = require('./handlers/errorHandlers');
@@ -35,6 +34,7 @@ app.use(cors())
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(session({
   secret: process.env.SECRET,
@@ -61,58 +61,6 @@ if (ENV === 'DEV') {
 }
 
 app.use(errorHandlers.productionErrors);
-
-// app.get('/', function(req, res) {
-  
-//   sessions.getAll({ pageSize: pageSize, startPage: 0 }).then((data) => {
-//     const totalPages = Math.ceil(data.totalSessions / pageSize);
-//     res.render('index.html', {
-//       pageNum: 1,
-//       totalPages: totalPages,
-//       totalSessions: data.totalSessions,
-//       sessions: data.sessions
-//     });
-//   });
-// });
-
-// app.get('/page/:page', function(req, res) {
-//   const pageNum = req.params.page;
-//   sessions.getAll({ pageSize: pageSize, startPage: +pageNum - 1 }).then((data) => {
-//     const totalPages = Math.ceil(data.totalSessions / pageSize);
-//     res.render('index.html', {
-//       pageNum: pageNum,
-//       totalPages: totalPages,
-//       totalSessions: data.totalSessions,
-//       sessions: data.sessions.map(session => {
-//         return Object.assign({}, session, { date: getFormattedDate(session.date) });
-//       })
-//     });
-//   });
-// });
-
-// app.get('/errors', function(req, res) {
-//   sessions.getAllWithErrors().then((sessions) => {
-//     res.render('errors.html', {
-//       totalSessions: sessions.length,
-//       sessions: sessions.map(session => {
-//         return Object.assign({}, session, { date: getFormattedDate(session.date) });
-//       })
-//     });
-//   });
-// });
-
-// app.get('/session/:id', function(req, res) {
-//   const sessionId = req.params.id;
-//   sessions.getById(sessionId, (err, session) => {
-//     res.render('session.html', {
-//       error: err,
-//       messages: session.messages.map(message => {
-//         return Object.assign({}, message, { date: getFormattedDate(message.date) });
-//       }),
-//       date: session.date
-//     });
-//   });
-// });
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
